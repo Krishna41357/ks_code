1d2cbf8e6a508205f2871a2a62c7383d8be78101
Krishna Srivastava <154420450+Krishna41357@users.noreply.github.com>
2025-03-23 21:28:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,11 +5,14 @@ const {Server} = require("socket.io");
 require("dotenv").config();
 const cors = require("cors");
 app.use(cors);
-
 app.use(express.json());
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server,{
+    cors: {
+      origin: "https://your-frontend.vercel.app",
+      methods: ["GET", "POST"],
+    }});
 
 const PORT = process.env.PORT || 5000;
 
